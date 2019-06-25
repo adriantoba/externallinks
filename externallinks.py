50e942ba0d73a27c64cf4ac5331d5710bb68db4b
adriantoba <40370047+adriantoba@users.noreply.github.com>
2019-06-26 02:53:07 +0300

    Add files via upload

---
+++ externallinks.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import re
+import urllib.request
+
+class AppURLopener(urllib.request.FancyURLopener):
+    version = "Mozilla/5.0"
+
+def linkExtractor(url):
+
+    opener = AppURLopener()
+
+    resp = opener.open(url)
+
+    soup = BeautifulSoup(resp, from_encoding=resp.info().get_param('charset'), features="html.parser" )
+
+    a = []
+
+    for link in soup.find_all('a', attrs={'href': re.compile("^http://")}): #for only the external links
+    
+    #for link in soup.find_all('a', href=True):    #for all the links
+        
+       a.append(link['href'])
+
+    return a
+
+    
+print('\n'.join(map(str,linkExtractor("http://iasiexpert.ro/casa-si-gradina"))))
+
+
+
+
+
+
+
+
+
